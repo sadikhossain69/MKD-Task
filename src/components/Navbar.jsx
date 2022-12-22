55f72ec4fb523b42cf7fe101d0fd5a38fc640082
sadikhossain69 <sadikhossain433@gmail.com>
2022-12-23 00:26:04 +0600

    logout system done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <div className='flex justify-between items-center px-6'>
             <div className='text-4xl font-bold'>APP</div>
-            <div className='px-7 rounded-full font-medium py-2 bg-[#9BFF00] text-black my-2
-            '>Logout</div>
+            <Link onClick={() => localStorage.clear()} to={'/admin/login'}>
+                <div className='px-7 rounded-full font-medium py-2 bg-[#9BFF00] text-black my-2
+            '>{
+                localStorage.getItem('email') && "Logout"
+            }</div>
+            </Link>
         </div>
     );
 };
